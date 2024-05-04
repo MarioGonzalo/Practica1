721605f2e374957eb31bd6c8d606c83ab63183f5
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2024-05-04 18:34:50 +0200

    Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Experimento exp = new Experimento(1, "medio1");
+        PoblacionBacteria poblacion = new PoblacionBacteria("bacteria1", exp, null, null, 100, 100, 37, null);
+        exp.addPoblacBacteria(poblacion);
+        System.out.println(poblacion.obtenerCantidadComidaDia(5));
+        poblacion.alimentar(5);
+        System.out.println(poblacion.numActualBacteria);
+        System.out.println(poblacion.getFechaFin());
     }
 }
